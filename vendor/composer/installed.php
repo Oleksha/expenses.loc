9be2217dd9587f9e15017182e68e150aacf1df0e
Oleksha <deryabino@gmail.com>
2023-12-25 19:27:54 +0400

    Создали структуру приложения MVC

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'expenses/expenses.loc',
+        'pretty_version' => 'dev-origin',
+        'version' => 'dev-origin',
+        'reference' => '5e4b5ef25b308281c2a60c7a8fc472fd0a30b9ff',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'expenses/expenses.loc' => array(
+            'pretty_version' => 'dev-origin',
+            'version' => 'dev-origin',
+            'reference' => '5e4b5ef25b308281c2a60c7a8fc472fd0a30b9ff',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
